608a20b2a172544ab1b4f7fe8aa94e33f821bb1e
Ian Fore <forei@mail.nih.gov>
2010-10-25 11:12:25 +0000

    Various changes for caTissue ETL to the CBM tables. Not yet complete but checking in to get input from others.
    SVN-Revision: 69

---
+++ ETL/data/mapping tables.sql
@@ -0,0 +1,36 @@
+create table mappings
+(term_id integer,
+CBM_Name varchar(255),
+NCI_Code varchar(50),
+NCI_Name varchar(255),
+Source varchar(255),
+Source_Code varchar(50),
+Source_concept_name varchar(255)
+);
+
+
+create table Clinical_Diagnosis_SCG
+(Permissible_Value varchar(255) ,
+SNOMED_Code varchar(50)  PRIMARY KEY 
+);
+
+CREATE INDEX Diagnosis_value
+    ON Clinical_Diagnosis_SCG (Permissible_Value);
+    
+create table anatomic_site_mapping (
+    Code varchar(50) primary key,
+    Site	 varchar(255) unique,
+    Parent varchar(50),
+    ICD_Code varchar(50),
+    NCI_Meta_CUI varchar(50),
+    NCI_Code varchar(50)
+    );
+    
+CREATE INDEX anatomic_site_value
+ON anatomic_site_mapping (Site);
+
+CREATE INDEX anatomic_site_value
+ON anatomic_site_mapping (Site);
+
+CREATE INDEX anatomic_site_value
+ON anatomic_site_mapping (Site);
