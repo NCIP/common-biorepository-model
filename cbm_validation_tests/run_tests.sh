6f05ad5a2485a485c7b21138415bb65f4ff79a2c
Benjamin Powers <PowersB@imsweb.com>
2011-04-14 20:08:36 +0000

    Update to execution scripts and README file
    SVN-Revision: 138

--- cbm_validation_tests/run_tests.sh
+++ cbm_validation_tests/run_tests.sh
@@ -2,12 +2,16 @@
 
 export CBM_TEST_DIR=.
 export LIB_DIR=$CBM_TEST_DIR/lib
-export JAVA_HOME=/usr/lib/java
-export ANT_HOME=$CBM_TEST_DIR/apache-ant-1.7.0
 export classpath=./bin:$LIB_DIR/*
-CBM_URL=http://cabigapps01.cancer.gov:9294/wsrf/services/cagrid/CBM
 TEST_OUTPUT_DIRECTORY=./data
 TEST_REPORT_DIRECTORY=./report
+
+
+#Modify to suit environment
+export JAVA_HOME=/usr/lib/java
+export ANT_HOME=$CBM_TEST_DIR/apache-ant-1.7.0
+CBM_URL=http://cabigapps01.cancer.gov:9294/wsrf/services/cagrid/CBM
+
 
 
 echo $classpath
